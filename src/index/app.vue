1b175bd95f83692a446a0c6b6301d5424084e50c
Romez1990 <Romez19900@gmail.com>
2019-02-16 17:52:48 +0300

    Covert html to pug and scss to stulys

--- src/index/app.vue
+++ src/index/app.vue
@@ -1,77 +1,55 @@
-<template>
-	<div id="wrapper">
-		<form>
-			<h1>Registration</h1>
-			<field type="text"
-			       name="login"
-			       placeholder="Login"
-			       errormsg="Enter your login"></field>
-			<field type="password"
-			       name="password"
-			       placeholder="Password"
-			       errormsg="Enter your password"></field>
-			<field type="password"
-			       name="password-confirmation"
-			       placeholder="Password confirmation"
-			       errormsg="Confirm your password"></field>
-			<input type="button" id="submit-button" value="Next">
-		</form>
-	</div>
+<template lang="pug">
+	#wrapper
+		form
+			h1 Registration
+			field(type='text', name='login', placeholder='Login', errormsg='Enter your login')
+			field(type='password', name='password', placeholder='Password', errormsg='Enter your password')
+			field(type='password', name='password-confirmation', placeholder='Password confirmation', errormsg='Confirm your password')
+			input#submit-button(type='button', value='Next')
 </template>
 
 <style lang="stylus">
 	@import '../components/standard.styl'
 	
-	body {
-		font-family: sans-serif;
-	}
+	body
+		font-family sans-serif
 	
-	#wrapper {
-		height: 100vh;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-	}
+	#wrapper
+		height 100vh
+		display flex
+		justify-content center
+		align-items center
 	
-	form {
-		padding: 40px 30px;
-		border-radius: 5px;
-		box-shadow: 1px 2px 5px 2px rgba(0, 0, 0, 0.22);
-		display: flex;
-		flex-direction: column;
+	form
+		padding 40px 30px
+		border-radius 5px
+		box-shadow 1px 2px 5px 2px rgba(0, 0, 0, 0.22)
+		display flex
+		flex-direction column
 		
-		h1 {
-			margin-bottom: 5px;
-		}
+		h1
+			margin-bottom 5px
 		
-		#submit-button {
+		#submit-button
 			nice-button(#08f, #eee, #023)
-			margin-top: 4px;
-			align-self: flex-end;
-		}
-	}
+			margin-top 4px
+			align-self flex-end
 	
-	@media screen and (max-width: 368px) {
-		#wrapper {
-			display: block;
-		}
+	@media screen and (max-width 368px)
+		#wrapper
+			display block
 		
-		form {
-			padding: 10px 15px;
-			box-shadow: none;
-		}
-	}
+		form
+			padding 10px 15px
+			box-shadow none
 	
-	@media screen and (max-height: 430px) {
-		#wrapper {
-			display: block;
-		}
+	@media screen and (max-height 430px)
+		#wrapper
+			display block
 		
-		form {
-			padding: 10px 15px;
-			box-shadow: none;
-		}
-	}
+		form
+			padding 10px 15px
+			box-shadow none
 </style>
 
 <script>
